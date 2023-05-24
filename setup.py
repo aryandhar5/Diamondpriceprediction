a367b628e679aa8f4470b58f9b8e4860c09ca488
aryandhar5 <aryandhar18@gmail.com>
2023-05-25 01:52:50 +0530

    Second commit

--- setup.py
+++ setup.py
@@ -6,32 +6,22 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
+     requirements=file_obj.readlines()
+     requirements=[req.replace("\n","") for req in requirements]
+
+     if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
-        return requirements
- 
+
+            return requirements
+
+     return requirements
+
 
 setup(
-    name='DiamondPricePridiction',
+    name='DiamondPricePrediction',
     version='0.0.1',
-    author='Aryan',
-    author_email='aryandhar@gmail.com',
+    author='Pwskills',
+    author_email='example@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
-
-
-
- )
-
-
-
-
-
-
-
-
-
+)
